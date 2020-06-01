838f1acc70733d08fa9bc5ada93c1da6790508f5
Bwolfs2 <bwolfnoob@gmail.com>
2020-05-31 21:34:58 -0300

    export hasura_core_base

--- lib/hasura_core.dart
+++ lib/hasura_core.dart
@@ -1,6 +1,7 @@
 library hasura_core;
 
 export 'src/core/hasura_core.dart';
+export 'src/core/hasura_core_base.dart';
 export 'src/exceptions/hasura_error.dart';
 export 'src/services/local_storage.dart';
 export 'src/snapshot/snapshot.dart';
